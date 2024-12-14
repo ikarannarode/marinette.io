2348dab9b6f1902cb90ba95b182fc89a808cf25b
KARAN'S MAC <karannarode@CodeZ-MacBook-Air.local>
2024-12-14 19:07:23 +0530

    Add Typing effect

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -13,24 +13,34 @@ const Main = () => {
                 <img src={assets.user_icon} alt="" />
             </div>
             <div className="main-container">
-                <div className="greet">
-                    <p><span>Hello, Karan</span></p>
-                    <p>How can I help you today?</p>
-                </div>
-                <div className="cards">
-                    <div className="card"><p>Suggest beatiful places to see on an upcomming road trip</p>
-                        <img src={assets.compass_icon} alt="" />
+                {!showResult ? <>
+                    <div className="greet">
+                        <p><span>Hello, Karan</span></p>
+                        <p>How can I help you today?</p>
                     </div>
-                    <div className="card"><p>Briefly summarize this concept: urban planning</p>
-                        <img src={assets.bulb_icon} alt="" />
+                    <div className="cards">
+                        <div className="card"><p>Suggest beatiful places to see on an upcomming road trip</p>
+                            <img src={assets.compass_icon} alt="" />
+                        </div>
+                        <div className="card"><p>Briefly summarize this concept: urban planning</p>
+                            <img src={assets.bulb_icon} alt="" />
+                        </div>
+                        <div className="card"><p>Brainstorm team bonding activites for our work retreat</p>
+                            <img src={assets.message_icon} alt="" />
+                        </div>
+                        <div className="card"><p>Improve the readability of the following code</p>
+                            <img src={assets.code_icon} alt="" />
+                        </div>
+                    </div></> : <div className='result'><div className="result-title"><img src={assets.user_icon} alt="" />
+                        <p>{recentPrompt}</p>
                     </div>
-                    <div className="card"><p>Brainstorm team bonding activites for our work retreat</p>
-                        <img src={assets.message_icon} alt="" />
-                    </div>
-                    <div className="card"><p>Improve the readability of the following code</p>
-                        <img src={assets.code_icon} alt="" />
-                    </div>
-                </div>
+                    <div className="result-data"><img src={assets.gemini_icon} alt="" />
+                        {loading ? <div className='loader'>
+                            <hr />
+                            <hr />
+                            <hr />
+                        </div> : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>}
+                    </div></div>}
                 <div className="main-bottom">
                     <div className="search-box">
                         <input type="text" onChange={(e) => setInput(e.target.value)} value={input} name="" placeholder='Enter a Prompt here' id="" />
@@ -46,7 +56,7 @@ const Main = () => {
                 </div>
             </div>
 
-        </div>
+        </div >
     )
 }
 
