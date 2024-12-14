98ea871ecf7188b202326cac7698ba48d4f37a2e
KARAN'S MAC <karannarode@CodeZ-MacBook-Air.local>
2024-12-14 05:42:07 +0530

    UI Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/SideBar';
+import Main from './components/Main/Main';
+
 
 const App = () => {
   return (
     <>
       <Sidebar />
+      <Main />
     </>
   )
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import "./Main.css"
+import { assets } from '../../assets/assets'
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className="nav">
+                <p>marinette.io</p>
+                <img src={assets.user_icon} alt="" />
+            </div>
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello, Karan</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card"><p>Suggest beatiful places to see on an upcomming road trip</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card"><p>Briefly summarize this concept: urban planning</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card"><p>Brainstorm team bonding activites for our work retreat</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card"><p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" name="" placeholder='Enter a Prompt here' id="" />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className='bottom-info'>
+                        marinette.io can make mistakes. Check important info.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Main
